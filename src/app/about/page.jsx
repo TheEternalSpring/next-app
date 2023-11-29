07270e83252d0181e309ee2e0126a4391043f2da
TheEternalSpring <sababuch@gmail.com>
2023-11-29 21:29:25 +0400

    cleaneds tuff and added portfolio and such

---
+++ src/app/about/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Abowt() {
+  return (
+    <div>About us</div>
+  )
+}
+
+export default Abowt
---
+++ src/app/redundancydepartmentredundacy/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AJAJAJR() {
+  return (
+    <div>REDUNDANT</div>
+  )
+}
+
+export default AJAJAJR
